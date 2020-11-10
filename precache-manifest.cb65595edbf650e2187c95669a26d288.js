f233477c0c0019751f3e2a92de4de017f81064cc
msihly <43391570+msihly@users.noreply.github.com>
2020-11-09 19:39:32 -0500

    Updates

---
+++ precache-manifest.cb65595edbf650e2187c95669a26d288.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145bfdb7ab4b294a6746e74bb7f42727",
+    "url": "/Letter-Tiles/index.html"
+  },
+  {
+    "revision": "3e990405faf04f9eada5",
+    "url": "/Letter-Tiles/static/css/main.6fa96775.chunk.css"
+  },
+  {
+    "revision": "4985146b9908e373cf7d",
+    "url": "/Letter-Tiles/static/js/2.707d03e0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Letter-Tiles/static/js/2.707d03e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e990405faf04f9eada5",
+    "url": "/Letter-Tiles/static/js/main.30417b61.chunk.js"
+  },
+  {
+    "revision": "d3d0f45d59f8bf8cc678",
+    "url": "/Letter-Tiles/static/js/runtime-main.ff956256.js"
+  },
+  {
+    "revision": "70bf025acb5657161dd5f2742bc16a1c",
+    "url": "/Letter-Tiles/static/media/eraser.70bf025a.svg"
+  },
+  {
+    "revision": "9192693563353b2290bca7b191d245cf",
+    "url": "/Letter-Tiles/static/media/grid.91926935.svg"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/Letter-Tiles/static/media/roboto-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/Letter-Tiles/static/media/roboto-400.60fa3c06.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/Letter-Tiles/static/media/roboto-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/Letter-Tiles/static/media/roboto-500.87284894.woff"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/Letter-Tiles/static/media/roboto-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/Letter-Tiles/static/media/roboto-700.adcde98f.woff"
+  },
+  {
+    "revision": "11a7a7571f494fa5526eec201cbb50d0",
+    "url": "/Letter-Tiles/static/media/trashcan.11a7a757.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/letter-tiles/precache-manifest.70ec43589b3177bc86618ee1388570d2.js"
+  "/Letter-Tiles/precache-manifest.cb65595edbf650e2187c95669a26d288.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/letter-tiles/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Letter-Tiles/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
